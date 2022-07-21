f8f5dcf9fc6b9425d4bbcff3122a719179061d36
Lucas S <lucas.sorribes@gmail.com>
2022-07-21 18:49:14 +0200

    Add 6_kuy 01.rb

--- 6_kyu/01.rb
+++ 6_kyu/01.rb
@@ -5,8 +5,8 @@
 # Given a list and a number, create a new list that contains each number of list
 # at most N times, without reordering.
 
-# For example if the input number is 2, and the input list is [1,2,3,1,2,1,2,3], 
-# you take [1,2,3,1,2], drop the next [1,2] since this would lead to 1 and 2 
+# For example if the input number is 2, and the input list is [1,2,3,1,2,1,2,3],
+# you take [1,2,3,1,2], drop the next [1,2] since this would lead to 1 and 2
 # being in the result 3 times, and then take 3, which leads to [1,2,3,1,2,3].
 # With list [20,37,20,21] and number 1, the result would be [20,37,21].
 
@@ -31,7 +31,6 @@
 
 # case emtpy array ?? // single element array ??
 
-# case
 
 # Algorithm
 
@@ -48,13 +47,16 @@
 
 # 3. RETURN `output_array`
 
-def delete_nth(input_array, n)
+
+def delete_nth(input_array, max_occurrences)
 
   input_array.each_with_object([]) do |integer, output_array|
     output_array << integer
-    output_array.pop if output_array.count(integer) > n
+    output_array.pop if output_array.count(integer) > max_occurrences
   end
 
 end
 
 p delete_nth([1,2,3,1,2,1,2,3], 2) == [1, 2, 3, 1, 2, 3]
+
+p delete_nth([20,37,20,21], 1) == [20,37,21]
